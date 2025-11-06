d607af7f5a7162a3a98e642dde735194725853cb
Isaac Hoyos <gamemasterx40@gmail.com>
2025-11-05 19:55:32 -0500

    Add files via upload

---
+++ auto-update-vscode.bash
@@ -0,0 +1,5 @@
+# This script will auto-update Visual Studio Code for Chromebook in the terminal when the current version is outdated.
+
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/code_latest_amd64.deb
+
+sudo dpkg -i /tmp/code_latest_amd64.deb
